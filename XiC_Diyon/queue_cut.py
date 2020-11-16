d626f136894c7a67fc591d963c1ea382d06f4ca7
Diyon Wickremeratne <dwickrem@stbc-i1.nikhef.nl>
2020-11-16 13:07:41 +0100

    moved all files to new branch

---
+++ XiC_Diyon/queue_cut.py
@@ -0,0 +1,32 @@
+"""
+This script is similar to queue_stoomboot.py, only it is used to queue the
+data cutting script.
+
+Author: Maris Koopmans
+"""
+
+import os, sys
+
+# Define the python script which need to be runned
+run_cmd = 'python /user/dwickrem/documents/XiC_analysis/cut_data.py'
+
+# Define the output directory
+output_dir = f'/data/bfys/dwickrem/sig_bkg_folder/'
+
+# Define a temporary bash file to execute the cut script
+submit_script = 'execute_stoomboot_cut.sh'
+
+# Add the command to the submit file
+os.system(f'echo "#!/bin/sh" >> {submit_script}')
+
+# Move into the directory for the current run
+os.system(f'echo cd {output_dir} >> {submit_script}')
+
+os.system(f'echo {run_cmd} >> {submit_script}')
+
+# Define the command to run the script on the cluster and run it
+run_command = f"qsub -j oe -o /data/bfys/dwickrem/sig_bkg_folder/cut_data_final_vars_sigsmall.out {submit_script}"
+os.system(run_command)
+
+# Delete the temporary bash file to keep the directory clean
+os.system(f"rm -f {submit_script}")
---
+++ XiC_Diyon/queue_stoomboot.py
@@ -0,0 +1,46 @@
+"""
+This script is used to run the BDT python script on the stoomboot cluster
+
+Author: Maris Koopmans
+"""
+
+import os, sys
+
+# Indicate which run we're testing
+run = 22
+
+# Boost the tree with some different amount of trees
+ntrees = [100]#, 200, 400, 650, 800, 1200, 2000]
+# ntrees = [200, 400, 650]
+
+# Define and create the output directory
+output_dir = f'/data/bfys/dwickrem/bdt_train_outputs/BDT_run{run}'
+
+try:
+    os.mkdir(output_dir)
+except OSError:
+    print(f'creation of directory {output_dir} failed')
+
+for ntree in ntrees:
+
+    # Define the command we want to use
+    test_cmd = f'python /user/dwickrem/documents/XiC_analysis/load_and_train.py {run} {ntree} {output_dir}'
+
+    # Define the script we want to use to execute this command
+    submit_script = 'execute_stoomboot.sh'
+
+    # Add the command to the submit file
+    os.system(f'echo "#!/bin/sh" >> {submit_script}')
+    os.system(f'echo ". /user/dwickrem/.bashrc">>{submit_script}')
+
+    # Move into the directory for the current run
+    os.system(f'echo cd {output_dir} >> {submit_script}')
+    os.system(f'echo {test_cmd} >> {submit_script}')
+
+    # Define the command to run the script on the cluster and run it
+    # Run on the gpu queue as the script needs to be allocated a lot of RAM
+    run_command = f"qsub -q gpu -o /data/bfys/dwickrem/bdt_train_outputs/BDT_run{run}/run_{run}_{ntree}trees.out -e {output_dir}/log.txt {submit_script}"
+    os.system(run_command)
+
+    # Delete the file to execute the script to keep the directory clean
+    os.system(f"rm -f {submit_script}")
